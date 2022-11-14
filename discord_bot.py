3e00b95678a8d0ef65e84d18a529663653da3a1a
cdfisher <cdfisher96@gmail.com>
2022-11-14 13:04:40 -0500

    Minor restructure to break out parsing of commands for cleaner code

--- discord_bot.py
+++ discord_bot.py
@@ -29,42 +29,44 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.lower()[:4] == '!cmb':
-        rsn = message.content[5:]
+    cmd, body = parse_command(message.content)
+
+    if cmd == '!cmb':
+        rsn = body
 
         levels = calc_cmb_lvl(rsn)
 
         response = '{} has a combat level of {}\n' \
-                   'Attack: {}\n' \
-                   'Defence: {}\n' \
-                   'Strength: {}\n' \
-                   'Hitpoints: {}\n' \
-                   'Ranged: {}\n' \
-                   'Prayer: {}\n' \
-                   'Magic: {}\n'.format(rsn, levels[0],
-                                        levels[1],
-                                        levels[2],
-                                        levels[3],
-                                        levels[4],
-                                        levels[5],
-                                        levels[6],
-                                        levels[7])
+                   'Attack:    {:<2}\n' \
+                   'Defence:   {:<2}\n' \
+                   'Strength:  {:<2}\n' \
+                   'Hitpoints: {:<2}\n' \
+                   'Ranged:    {:<2}\n' \
+                   'Prayer:    {:<2}\n' \
+                   'Magic:     {:<2}\n'.format(rsn, levels[0],
+                                               levels[1],
+                                               levels[2],
+                                               levels[3],
+                                               levels[4],
+                                               levels[5],
+                                               levels[6],
+                                               levels[7])
 
         await message.channel.send(response)
         print(f'Message sent: "{response}"/n')
 
-    elif message.content.lower()[:3] == '!hs':
-        rsn = message.content[4:]
+    elif cmd == '!hs':
+        rsn = body
 
         get_hs(rsn)
 
         file = rsn + '.txt'
-        file_payload = discord.File(file, filename=(rsn+'.txt'))
+        file_payload = discord.File(file, filename=(rsn + '.txt'))
         await message.channel.send(f'{rsn}\'s OSRS Highscores:\n', file=file_payload)
         os.remove(file)
 
-    elif message.content.lower()[:7] == '!skills':
-        rsn = message.content[8:]
+    elif cmd == '!skills':
+        rsn = body
 
         get_skills(rsn)
 
@@ -73,8 +75,8 @@ async def on_message(message):
         await message.channel.send(f'{rsn}\'s OSRS Skills:\n', file=file_payload)
         os.remove(file)
 
-    elif message.content.lower()[:7] == '!bosses':
-        rsn = message.content[8:]
+    elif cmd == '!bosses':
+        rsn = body
 
         get_bosses(rsn)
 
@@ -83,8 +85,8 @@ async def on_message(message):
         await message.channel.send(f'{rsn}\'s OSRS Boss KC:\n', file=file_payload)
         os.remove(file)
 
-    elif message.content.lower()[:4] == '!ehb':
-        rsn = message.content[5:]
+    elif cmd == '!ehb':
+        rsn = body
 
         calc_ehb(rsn)
 
@@ -93,8 +95,8 @@ async def on_message(message):
         await message.channel.send(f'{rsn}\'s OSRS efficient hours bossed:\n', file=file_payload)
         os.remove(file)
 
-    elif message.content.lower()[:11] == '!activities':
-        rsn = message.content[12:]
+    elif cmd == '!activities':
+        rsn = body
 
         get_activities(rsn)
 
@@ -103,12 +105,20 @@ async def on_message(message):
         await message.channel.send(f'{rsn}\'s OSRS Activities:\n', file=file_payload)
         os.remove(file)
 
-    elif message.content.lower()[:3] == '!:p':
+    elif cmd == '!:p':
         await message.channel.send(':stuck_out_tongue_winking_eye:\n'
                                    '***__THBBBBBBBBBBBBBBBT!!!!__***')
 
-    elif message.content.lower()[:8] == '!version':
+    elif cmd == '!version':
         await message.channel.send(f'Running Clockwork Penguin {VERSION}\n')
+
+
+def parse_command(content):
+    if ' ' not in content:
+        return content.lower(), ''
+    else:
+        cmd, body = content.split(' ', maxsplit=1)
+        return cmd.lower(), body
 
 
 client.run(TOKEN)
