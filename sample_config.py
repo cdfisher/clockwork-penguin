4a719c0f4c3660255642ab97baf4c65414c978f5
cdfisher <cdfisher96@gmail.com>
2022-11-14 13:56:38 -0500

    Increment version number

--- sample_config.py
+++ sample_config.py
@@ -2,7 +2,7 @@
 Constants for use by the Clockwork Penguin Discord bot.
 Add bot token and server name to this file and rename to 'constants.py' before use
 """
-VERSION='v0.1 alpha'
+VERSION='v0.2 alpha'
 
 
 DISCORD_TOKEN=''
