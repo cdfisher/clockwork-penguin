666b16e2709c530a7cfc365b18b6527df8a897ed
cdfisher <cdfisher96@gmail.com>
2022-12-04 22:46:38 -0500

    Updates sample config with new values

--- sample_config.py
+++ sample_config.py
@@ -1,17 +1,25 @@
-"""sample_constants.py
+"""sample_config.py
 Constants for use by the Clockwork Penguin Discord bot.
 Add bot token and server name to this file and rename to 'constants.py' before use
 """
 
 TEST_MODE = 0
 
-VERSION = 'v0.3 alpha'
+VERSION = 'v0.3.1 alpha'
+# Default avatar to use when sending messages via webhooks
+AVATAR_URL = 'https://github.com/cdfisher/clockwork-penguin/blob/master/resources/icon.png?raw=true'
+# Default bot name to use when sending messages via webhooks
+BOT_NAME = 'Clockwork Penguin'
+# Name to be used for log files
+LOG_NAME = 'clockwork-penguin-log.txt'
 
 DISCORD_TOKEN = ''
 DISCORD_GUILD = ''
 
 # Webhooks
-BIRDMEN_WEBHOOK = ''
+WEBHOOK = ''
+
+BIRDMAN_AVATAR = 'https://github.com/cdfisher/clockwork-penguin/blob/master/resources/kreearra.png?raw=true'
 
 # Test mode values
 TEST_TOKEN = ''
